10938ea8ad744c3c3c8a5d68ff79c7e45f9677fa
Saijami38 <saijami38.com>
2023-08-06 11:19:11 +0530

    Routing added

---
+++ src/Components/toolkit/UserDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserDetails = () => {
+  return (
+    <>
+      <h2 className="admin-subtitle">List of Users</h2>
+      <button>Add New Users</button>
+    </>
+  );
+};
+
+export default UserDetails;
